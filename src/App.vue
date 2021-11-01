a4b0462a9a33d76882b8a7a27c1ce2a23217e74a
NazarovCC <sergosnaz23@gmail.com>
2021-11-01 16:55:31 +0300

    add newproduct

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
   <add-product-modal v-if="showModal" @close="closeModal">
     <template #header><h3>Modalka</h3></template>
-    <template #body><input type="text" name="zzz" id="lll" /></template>
-    <template #footer><button class="btn btn-primary">Отправить</button></template>
+    <template #body>
+      <label v-if="productName.length ===0" for="name">Введите имя продукта</label>
+      <br>
+      <input type="text" id="name" v-model.trim="productName" />
+
+
+    </template>
+    <template #footer><button :disabled="productName.length ===0" class="btn btn-primary" @click="sendEvent">Отправить</button></template>
   </add-product-modal>
   <div class="container text-center">
     <router-view @open="showModal=true"/>
@@ -10,9 +16,10 @@
 </template>
 
 <script>
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import {useStore} from 'vuex'
 import AddProductModal from "./components/AddProductModal";
+import { useRoute } from 'vue-router';
 
 export default {
   components: {
@@ -20,7 +27,13 @@ export default {
   },
   setup() {
     const store = useStore()
+    const route = useRoute()
+    const productName = ref('')
     const showModal = computed(()=>store.getters['session/getShowModal'])
+    const sendEvent = () =>{
+      store.commit('session/addNewProduct',{name:productName.value, id:route.params.id})
+      store.commit('session/changeShowModal',false)
+    }
     const closeModal = ()=>{
       store.commit('session/changeShowModal',false)
 
@@ -28,6 +41,8 @@ export default {
     return {
       showModal,
       closeModal,
+      sendEvent,
+      productName,
     };
   },
 };
